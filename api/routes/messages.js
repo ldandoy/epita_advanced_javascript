97318e98b0d32183573dcc296bc91dd7c457f0ab
overc <ldandoy@overconsulting.net>
2022-01-21 10:25:17 +0100

    Add check user to message

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -9,8 +9,14 @@ let messages = []
 
 router.get('/', auth, async (req, res) => {
     try {
-        messages = await messageModel.find({autor: req.session.user._id}).populate({ path: 'autor', select: 'username email' })
-        return res.status(200).json(messages)
+        if (req.session && req.session.user) {
+            messages = await messageModel.find({
+                autor: req.session.user._id
+            }).populate({ path: 'autor', select: 'username email' })
+            return res.status(200).json(messages)
+        } else {
+            return res.status(500).json('You are not connected !')
+        }
     } catch (error) {
         console.error(error)
         return res.status(500).json(error.message)
@@ -19,8 +25,15 @@ router.get('/', auth, async (req, res) => {
 
 router.get('/:messageId', auth, async (req, res) => {
     try {
-        let message = messageModel.findOne({_id: req.params.messageId})
-        return res.status(200).json(message)
+        if (req.session && req.session.user) {
+            let message = messageModel.findOne({
+                _id: req.params.messageId,
+                autor: req.session.user._id
+            }).populate({ path: 'autor', select: 'username email'})
+            return res.status(200).json(message)
+        } else {
+            return res.status(500).json('You are not connected !')
+        }
     } catch (error) {
         console.error(error)
         return res.status(500).json(error.message)
@@ -50,13 +63,18 @@ router.post('/', auth, async (req, res) => {
 
 router.put('/:messageId', auth, async (req, res) => {
     try {
-        const inputDatas = req.body
+        if (!req.session.user) {
+            return res.status(500).json({msg: "You have to login to modify this message !"})
+        }
 
-        let message = await messageModel.findOneAndUpdate(
-            { _id: req.params.messageId, autor: req.session.user._id },
-            inputDatas,
-            { new: true }
-        )
+        const { content } = req.body
+
+        let message = await messageModel.findOneAndUpdate({ 
+            _id: req.params.messageId,
+            autor: req.session.user._id
+        },
+        { content },
+        { new: true })
         
         return res.status(200).json(message)
     } catch (error) {
@@ -67,7 +85,14 @@ router.put('/:messageId', auth, async (req, res) => {
 
 router.delete('/:messageId', auth, async (req,res) => {
     try {
-        await messageModel.findOneAndDelete({_id: req.params.messageId})
+        if (!req.session.user) {
+            return res.status(500).json({msg: "You have to login to delete this message !"})
+        }
+
+        await messageModel.findOneAndDelete({
+            _id: req.params.messageId,
+            autor: req.session.user._id
+        })
         
         return res.status(200).json({msg: "Message deleted !"})
     } catch (error) {
